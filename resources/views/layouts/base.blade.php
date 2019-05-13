920c0d147f8ba8118662f662712835e8d5f0e440
Alexander Shchukin <1340068@gmail.com>
2019-05-13 11:59:20 +0300

    view layout: remove unused stack

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,9 +9,9 @@
 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 
-    @stack('styles')
 </head>
 <body class="{{ Route::currentRouteName() }}">
 
